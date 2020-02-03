94968f77b9421834cdebadd676ee316ec674dd35
zhengDaFeng <33956662+zhengDaFeng@users.noreply.github.com>
2020-02-03 20:20:21 +0800

    V1.0.0.0

---
+++ 笔趣阁爬虫/笔趣阁爬虫/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace 大风的笔趣阁爬虫
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(85, 28);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(18, 46);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(324, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(247, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "0/0";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(18, 94);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(324, 248);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.WordWrap = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(200, 17);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(142, 22);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "是否添加前缀";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 354);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "大风的笔趣阁爬虫";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ 笔趣阁爬虫/笔趣阁爬虫/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace 大风的笔趣阁爬虫
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Thread _ThreadMain;
+        bool _IsRunning = false;
+
+        /// <summary>
+        /// 根网址
+        /// </summary>
+        const string _strRootUrl = "https://www.biquge.com.cn";
+        /// <summary>
+        /// 小说ID
+        /// </summary>
+        string _strBookID = "";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(!_IsRunning)
+            {
+                Start();
+            }
+            else
+            {
+                Stop();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Stop();
+        }
+
+        /// <summary>
+        /// 检查小说ID是否正确
+        /// </summary>
+        /// <param name="id">小说ID</param>
+        /// <returns></returns>
+        private bool CheckBookID(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                MessageBox.Show("ID不能为空！");
+                return false;
+            }
+
+            var web = new HtmlAgilityPack.HtmlWeb();
+            var doc_book = web.Load($"{_strRootUrl}/book/{id}/");
+            HtmlAgilityPack.HtmlNode node_book_name = doc_book.DocumentNode.SelectSingleNode("//div[@id='info']/h1");
+            if (node_book_name == null)
+            {
+                MessageBox.Show($"找不到ID：{id}！");
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// 线程开始
+        /// </summary>
+        private void Start()
+        {
+            _strBookID = textBox1.Text;
+            if (!CheckBookID(_strBookID)) return;
+
+            _IsRunning = true;
+            button1.Text = "结束";
+            textBox1.Enabled = false;
+            checkBox1.Enabled = false;
+            _ThreadMain = new Thread(Run);
+            _ThreadMain.Start();
+        }
+
+        /// <summary>
+        /// 线程停止
+        /// </summary>
+        private void Stop()
+        {
+            if (_ThreadMain != null &&
+                _ThreadMain.ThreadState == ThreadState.Running)
+            {
+                _IsRunning = false;
+                textBox1.Enabled = true;
+                checkBox1.Enabled = true;
+                button1.Text = "开始";
+                progressBar1.Value = 0;
+                label2.Text = "0/0";
+            }
+        }
+
+        /// <summary>
+        /// 爬取线程
+        /// </summary>
+        private void Run()
+        {
+            var url = $"{_strRootUrl}/book/{_strBookID}/";
+            var web = new HtmlAgilityPack.HtmlWeb();
+            var doc_book = web.Load(url);
+
+            // 书名
+            HtmlAgilityPack.HtmlNode node_book_name = doc_book.DocumentNode.SelectSingleNode("//div[@id='info']/h1");
+            var book_name = node_book_name.InnerText;
+
+            // 章节数
+            HtmlAgilityPack.HtmlNodeCollection listNode = doc_book.DocumentNode.SelectNodes("//div[@id='list']/dl[1]/dd");
+            var count_chapter = listNode.Count;
+
+            // 初始化进度条
+            progressBar1.Invoke(new Action(() => {
+                textBox2.Text = "";
+                progressBar1.Maximum = count_chapter;
+                progressBar1.Value = 0;
+            }));
+
+            #region 章节循环
+
+            for (int i = 0; i < count_chapter; i++)
+            {
+                var tmp = listNode[i].FirstChild.Attributes["href"].Value;
+                var site_chapter = _strRootUrl + tmp;
+                var doc_chapter = web.Load(site_chapter);
+                HtmlAgilityPack.HtmlNode node_chapter_title = doc_chapter.DocumentNode.SelectSingleNode("//div[@class='bookname']/h1");
+
+                // 章节名
+                var title_chapter = "";
+                var isAppendFront = false;
+                this.Invoke(new Action(() => {
+                    isAppendFront = checkBox1.Checked;
+                }));
+                if (isAppendFront)
+                    title_chapter = "第" + (i + 1).ToString() + "章 " + node_chapter_title.InnerText;
+                else
+                    title_chapter = node_chapter_title.InnerText;
+
+                // 章节内容
+                HtmlAgilityPack.HtmlNode node_chapter_content = doc_chapter.DocumentNode.SelectSingleNode("//div[@id='content']");
+                var content_chapter = node_chapter_content.InnerText;
+                content_chapter = content_chapter.Replace("&nbsp;&nbsp;&nbsp;&nbsp;", "\r\n");
+
+                // 写入TXT
+                using (FileStream fs = new FileStream(book_name + ".txt", FileMode.Append, FileAccess.Write, FileShare.Read))
+                using (StreamWriter sw = new StreamWriter(fs, Encoding.UTF8))
+                {
+                    sw.WriteLine(title_chapter);
+                    sw.WriteLine(content_chapter);
+                }
+
+                // 更新进度
+                this.Invoke(new Action(() => {
+                    progressBar1.Value = i + 1;
+                    progressBar1.Update();
+                    label2.Text = $"{(i + 1).ToString()}/{count_chapter.ToString()}";
+                    textBox2.AppendText($"{title_chapter}\r\n");
+                }));
+
+                Thread.Sleep(1);
+
+                // 检查停止
+                if (!_IsRunning)
+                {
+                    this.Invoke(new Action(() => {
+                        progressBar1.Value = 0;
+                        progressBar1.Update();
+                        label2.Text = "0/0";
+                    }));
+                    return;
+                }
+            }
+
+            #endregion
+
+            MessageBox.Show("小说爬取完毕！");
+            this.Invoke(new Action(() => {
+                _IsRunning = false;
+                textBox1.Enabled = true;
+                checkBox1.Enabled = true;
+                button1.Text = "开始";
+                progressBar1.Value = 0;
+                label2.Text = "0/0";
+            }));
+        }
+    }
+}
